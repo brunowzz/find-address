415863389a17c0bfee4e9a34f51f72c6baa7c4d9
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2024-03-02 18:10:28 -0300

    feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RoutesApp from "./routes/routes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RoutesApp />
+  </React.StrictMode>
+);
