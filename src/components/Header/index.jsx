11991ea2afb6ea96c6989888a61c94484347fdbc
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2024-03-02 18:19:24 -0300

    feat: add layout

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Via Cep</h1>
+    </header>
+  );
+};
+
+export default Header;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import DefaultLayout from "../layout/DefaultLayout";
 import Home from "../pages/Home";
 
 const RoutesApp = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
